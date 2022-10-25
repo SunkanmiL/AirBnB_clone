d23dc1ce5ad41c7dc95aff762dece7d7b6f14985
Akacha Ubuntu <samchijioke16@gmail.com>
2022-10-25 02:38:27 +0100

    Created `to_dict(...)` method

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Defines a class ``BaseModel``"""
 import uuid
+from datetime import datetime
 
 
 class BaseModel:
@@ -26,13 +27,24 @@ class BaseModel:
 
         """
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime()
-        self.updated_at = datetime.datetime()
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def save(self):
         """Updates self.updated_at with the current datetime"""
-        self.updated_at = datetime.datetime()
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Returns a dictionary containing all keys/values of __dict__
+        of the instance
+        """
+        for key, value in self.__dict__.items():
+            my_dict[key] = value
+        my_dict['__class__'] = self.__class__.__name__
+        my_dict['created_at'] = self.created_at.isoformat()
+        my_dict['updated_at'] = self.updated_at.isoformat()
 
     def __str__(self):
         """Returns the string representation of an instance"""
-        return ("[BaseModel] ({}) {}".format(self.id, self.__dict__))
+        return ("[{}] ({}) {}".format(self.__class__.__name__,
+                self.id, self.__dict__))
