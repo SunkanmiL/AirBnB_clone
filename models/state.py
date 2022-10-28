acbe53c71c2b34f0152d048375756e2806b5daf0
SunkanmiL <oluwamuyiwasodiq@gmail.com>
2022-10-28 05:23:03 +0000

    Added state.py

---
+++ models/state.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Defines the State class."""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """State class inherits from BaseModel
+    Attribute:
+        name (str): Public class attribute for State's name
+    """
+
+    name = ""
