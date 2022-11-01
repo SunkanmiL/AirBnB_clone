8d66e6ce8b404f1ec31e236c596bfedb993b3f1e
SunkanmiL <oluwamuyiwasodiq@gmail.com>
2022-11-01 12:44:03 +0000

    Added place.py

--- models/place.py
+++ models/place.py
@@ -31,3 +31,11 @@ class Place(BaseModel):
     latitude = 0.0
     longitude = 0.0
     amenity_ids = []
+
+    def __init__(self, *args, **kwargs):
+        """init method for Place class
+        Attributes:
+            args (list): The list with arguments
+            kwargs (dict): A dictionary with arguments
+        """
+        super().__init__(*args, **kwargs)
