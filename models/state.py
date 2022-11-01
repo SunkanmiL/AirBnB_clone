9d15452a3833e0ef9bd1b9185f11a04409cd9469
SunkanmiL <oluwamuyiwasodiq@gmail.com>
2022-11-01 12:48:10 +0000

    Added state.py

--- models/state.py
+++ models/state.py
@@ -10,3 +10,11 @@ class State(BaseModel):
     """
 
     name = ""
+
+    def __init__(self, *args, **kwargs):
+        """init method for State class
+        Attributes:
+            args (list): The list with arguments
+            kwargs (dict): A dictionary with arguments
+        """
+        super().__init__(*args, **kwargs)
